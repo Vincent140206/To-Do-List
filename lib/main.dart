59187c61c97a237411a70d121264e038ccefcb5a
Vincent140206 <vincentiusdylan01@gmail.com>
2025-05-06 01:43:48 +0700

    Initialize the hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+import 'package:calendar_app/screens/auth/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:to_do_list/screens/auth/LoginPage.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 import 'core/event.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDir.path);
+
   Hive.registerAdapter(EventAdapter());
+
   await Hive.openBox<Event>('eventsBox');
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
